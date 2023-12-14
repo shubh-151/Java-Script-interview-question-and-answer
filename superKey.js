052066ab6ce875de75c301105bf23bc0aac27109
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-12-14 22:33:00 +0530

    super keyword

--- superKey.js
+++ superKey.js
@@ -1,3 +1,25 @@
 /* kabhi kabhi aisa kuch hota hai jha hm logo ko child se parent ke method ko 
 ya fir construtor ko call krna padhta hai to wha hm super ka use krte hain.*/
 
+class Person{
+    constructor(){
+        this.spacies = "homo sapiens";
+    }
+    
+    eat(){
+        console.log("eat")
+    }   
+}
+
+class Engineer extends Person{
+    constructor(branch){
+        super();//to invoke parent class constructor
+        super.eat()
+        this.branch = branch;
+    }
+    work(){
+        console.log("engineer work")
+    }
+}
+
+let Obj = new Engineer("chemical");

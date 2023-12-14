84b768ff22d8ea670dca5d19d7a48c2e5db3f92b
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-12-14 18:11:42 +0530

    Inheritence submittion

---
+++ Inheritence.js
@@ -0,0 +1,14 @@
+// Inheritence is passing down properties and method from parent to child
+
+class Parent{
+    hello(){
+        console.log("hello")
+    }
+
+}
+
+class Child extends Parent{
+    
+}
+
+let obj = new Child();
--- classAndObject.js
+++ classAndObject.js
@@ -1,13 +1,22 @@
-class ToyotaCar{
-    start(){
-        console.log("start");
-    }
-    stop(){
-        console.log("stop");
-    }
+class ToyotaCar {
+  //To create a construtor to do some initialization during run time
+  constructor(brand, mileage) {
+    console.log("create a object");
+    this.brand = brand;
+    this.mileage = mileage;
+  }
+  start() {
+    console.log("start");
+  }
+  stop() {
+    console.log("stop");
+  }
 }
 
 //To create a object for toyota car
 
-let fortuner = new ToyotaCar();
-let lexus = new ToyotaCar();
+let fortuner = new ToyotaCar("fortunner", 10);
+console.log(fortuner);
+let lexus = new ToyotaCar("lexus", 12);
+console.log(lexus)
+

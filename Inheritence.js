8dea9aa041993fb39ad66de2513c909ea5692854
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-12-14 20:20:26 +0530

    updated super keyword

--- Inheritence.js
+++ Inheritence.js
@@ -1,14 +1,51 @@
 // Inheritence is passing down properties and method from parent to child
 
-class Parent{
-    hello(){
-        console.log("hello")
+// class Parent{
+//     hello(){
+//         console.log("hello")
+//     }
+
+// }
+
+// class Child extends Parent{
+//     work(){
+//         console.log("work")
+//     }
+// }
+
+// let obj = new Child();
+
+
+
+
+class Person{
+
+    constructor(){
+        this.species = "homo sapiens";
     }
-
+    eat(){
+        console.log("eat")
+    }
+    sleep(){
+        console.log("sleep")
+    }
+    work(){
+        console.log("do nothing")
+    }
 }
 
-class Child extends Parent{
-    
+class Engineer extends Person{
+    work(){
+        console.log("engineer work")
+    }
 }
 
-let obj = new Child();
+
+
+class Doctor extends Person{
+    work(){
+        console.log("docter work")
+    }
+}
+
+let shubhamObj = new Person();
---
+++ superKey.js
@@ -0,0 +1,3 @@
+/* kabhi kabhi aisa kuch hota hai jha hm logo ko child se parent ke method ko 
+ya fir construtor ko call krna padhta hai to wha hm super ka use krte hain.*/
+

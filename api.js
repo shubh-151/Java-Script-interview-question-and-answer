f8e448d546890db4137926a3f647e766ab031b55
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-12-15 23:19:37 +0530

    api

--- api.js
+++ api.js
@@ -6,6 +6,16 @@
 
 //                  OR
 
+// console.log("one")
+// console.log("two")
+
 // setTimeout(() => {
 //     console.log("hello Api") 
 // }, 5000);
+
+// console.log("three")
+// console.log("four")
+
+// callback :- A callback is a function passed as an argument to another function
+// callback hell:- nested callback stacked below one function forming a pyramid structure
+

d1288aff4ec1d37411ef3aa019eef52da9b0d5e8
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-12-15 23:01:10 +0530

    setTimeout

---
+++ api.js
@@ -0,0 +1,11 @@
+// function hello() {
+//     console.log("hello Api")
+    
+// }
+// setTimeout(hello, 5000); // 5000ms = 5s
+
+//                  OR
+
+// setTimeout(() => {
+//     console.log("hello Api") 
+// }, 5000);

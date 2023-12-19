54b22c29dd42319cc6be5f4b0aefaa4af3cfbbb2
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-12-19 19:59:25 +0530

    updated value

--- fetchApi.js
+++ fetchApi.js
@@ -1,10 +1,14 @@
 const URL = 'https://cat-fact.herokuapp.com/facts';
 
+const catFact = document.querySelector("#fact");
+const btn = document.querySelector("#HitMe");
+
 const getData = async()=>{
-    console.log("getting data...")
     let res = await fetch(URL);
-    console.log(res);
     let data = await res.json();
-    console.log(data)
+    catFact.innerText = data[1].text;
+
 }
+btn.addEventListener("click", getData);
+
 
